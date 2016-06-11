e41752b8b3f6bb2db88ceaf423dc062325509f82
Myzreal <r.skupnik@gmail.com>
2016-06-12 01:17:13 +0200

    Add licensing information to pigeon-commons

---
+++ pigeon-commons/README.md
@@ -0,0 +1,1 @@
+Pigeon-commons is a library of common classes to be used with other pigeon projects.
--- pigeon-commons/src/main/java/com/github/rskupnik/pigeon/commons/exceptions/PigeonException.java
+++ pigeon-commons/src/main/java/com/github/rskupnik/pigeon/commons/exceptions/PigeonException.java
@@ -1,6 +1,24 @@
-package com.github.rskupnik.pigeon.commons.exceptions;
+/*
+    Copyright 2016 Radosław Skupnik
 
-import java.util.function.Supplier;
+    This file is part of pigeon-commons.
+
+    Pigeon-commons is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Pigeon-commons is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Pigeon-commons; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package com.github.rskupnik.pigeon.commons.exceptions;
 
 public class PigeonException extends Exception {
 
